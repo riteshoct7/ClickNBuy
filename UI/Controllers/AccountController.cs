f35c644e201a01901e5acd7548ba13d7503aac18
Ritesh Parekh <Riteshp@tblocks.com>
2021-04-27 20:13:03 +0530

    Added Login Model

---
+++ UI/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UI.Models;
+
+namespace UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public AccountController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ UI/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace UI.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "User Name Required")]
+        [DisplayName("User Name")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password Required")]
+        public string Password { get; set; }
+
+    }
+}
