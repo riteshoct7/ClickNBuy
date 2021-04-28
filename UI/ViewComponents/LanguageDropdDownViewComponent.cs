db45426c38bbb83e2baf5b8755ac67c30f0a997b
Ritesh Parekh <Riteshp@tblocks.com>
2021-04-28 17:19:32 +0530

    Added View Component

---
+++ UI/ViewComponents/LanguageDropdDownViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI.ViewComponents
+{
+    public class LanguageDropdDownViewComponent : ViewComponent
+    {
+        private readonly ICategoryService categoryService;
+
+
+        #region Constructors
+
+        public LanguageDropdDownViewComponent(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public IViewComponentResult Invoke()
+        {
+            var items = categoryService.GetAllCatefgories();
+            return View("~/Views/Shared/_LanguageDropdDownViewComponent.cshtml", items);
+        }
+
+        #endregion
+    }
+}
