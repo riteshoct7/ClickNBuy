586bc16a3bd31fe02a1614d7f11e4c3b060ae289
Ritesh Parekh <Riteshp@tblocks.com>
2021-04-28 12:16:53 +0530

    Fixes for api swagger not working

---
+++ WebApi/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Entity;
+using Microsoft.AspNetCore.Mvc;
+using Repository.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+
+        #region Properties
+
+        private readonly ICategoryRepository categoryRepository;
+        private readonly IMapper mapper; 
+
+        #endregion
+
+        #region Constructors
+        public CategoryController(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            this.categoryRepository = categoryRepository;
+            this.mapper = mapper;
+        }
+        #endregion
+
+        #region Methods
+
+        [HttpGet]
+        public IEnumerable<Category> GetAllCtaegories()
+        {
+            return categoryRepository.GetAll().ToList();
+        }
+
+        #endregion
+
+    }
+}
---
+++ WebApi/Helper/ApiApplicationMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Entity;
+using WebApi.Models;
+
+namespace WebApi.Helper
+{
+    public class ApiApplicationMapper : Profile
+    
+    {
+        #region Constructors
+        public ApiApplicationMapper()
+        {            
+            CreateMap<CategoryModel, Category>().ReverseMap();
+        }
+        #endregion
+    }
+}
